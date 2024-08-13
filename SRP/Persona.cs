fe66504a7a7f1244be1aa94e01f6b9d5b8230e56
beltranguz <beltranguzman4@gmail.com>
2024-08-13 16:05:04 -0400

    Agregar archivos de proyecto.

---
+++ SRP/Persona.cs
@@ -0,0 +1,29 @@
+﻿
+namespace SRP
+{
+    public class Persona
+    {
+        private string? nombre {  get; set; }
+        private int edad {  get; set; }
+        private string? direccion { get; set; }
+        private string? correoElectronico { get; set; }
+
+        public class EnviarCorreoElectronico
+        {
+            public decimal Enviar(string correo, string asunto, string mensaje)
+            
+            { 
+            // Logica para Enviar Correo
+            }
+        }
+
+    public void ImprimirDatos()
+        {
+            Console.WriteLine($"Nombre: {nombre}");
+            Console.WriteLine($"Edad: {edad}");
+            Console.WriteLine($"Direccion: {direccion}");
+            Console.WriteLine($"Correo Electronico: {correoElectronico}");
+
+        }
+    }
+}
